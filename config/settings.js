9538ac439c4096343e66c92714b941774fcc68c8
Keval <keval371@gmail.com>
2024-11-09 00:24:22 -0500

    mongo config

--- config/settings.js
+++ config/settings.js
@@ -1,6 +1,5 @@
 import dotenv from "dotenv";
 dotenv.config();
-console.log("MONGO_SERVER_URL:", process.env.MONGO_SERVER_URL); // Add this line to check
 export const mongoConfig = {
   serverUrl: process.env.MONGO_SERVER_URL,
   database: "ride-share",
