e55e9846c572cbf8af63670b22080eb9df56bbba
Keval <keval371@gmail.com>
2024-11-09 00:15:28 -0500

    Connection to mongodb Atlas

--- config/mongoConnection.js
+++ config/mongoConnection.js
@@ -1,17 +1,29 @@
-import { MongoClient } from 'mongodb';
-import { mongoConfig as _mongoConfig } from './settings';
+import { MongoClient, ServerApiVersion } from "mongodb";
+import { mongoConfig as _mongoConfig } from "./settings.js";
 const mongoConfig = _mongoConfig;
 
 let _connection = undefined;
 let _db = undefined;
 
-export default async () => {
+const client = new MongoClient(mongoConfig.serverUrl, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const dbConnection = async () => {
   if (!_connection) {
-    _connection = await MongoClient.connect(mongoConfig.serverUrl, {
-      useUnifiedTopology: true,
-    });
+    _connection = await client.connect();
+    // _db = await _connection.db(mongoConfig.database);
     _db = await _connection.db(mongoConfig.database);
   }
-
   return _db;
 };
+
+const closeConnection = async () => {
+  await _connection.close();
+};
+
+export { dbConnection, closeConnection };
--- config/settings.js
+++ config/settings.js
@@ -1,8 +1,9 @@
-
-require('dotenv').config();
-
+import dotenv from "dotenv";
+dotenv.config();
+console.log("MONGO_SERVER_URL:", process.env.MONGO_SERVER_URL); // Add this line to check
 export const mongoConfig = {
-  serverUrl: process.env.MONGO_SERVER_URL || 'mongodb://localhost:27017/',
-  database: process.env.MONGO_DB_NAME || 'rideSharingDB',
+  serverUrl: process.env.MONGO_SERVER_URL,
+  database: "ride-share",
 };
-export const sessionSecret = process.env.SESSION_SECRET || 'your_session_secret_here';
+// export const sessionSecret =
+//   process.env.SESSION_SECRET || "your_session_secret_here";
