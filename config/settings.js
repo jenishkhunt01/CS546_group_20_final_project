bc1054778f9241093d12844644184915d3f059d0
Keval <keval371@gmail.com>
2024-12-12 23:56:48 -0500

    Driver License Verification

--- config/settings.js
+++ config/settings.js
@@ -4,6 +4,7 @@ dotenv.config();
 export const mongoConfig = {
   serverUrl: process.env.MONGO_SERVER_URL || "mongodb://localhost:27017/",
   database: process.env.MONGO_DB_NAME || "ride-share",
+  imgBucket: "uploads",
 };
 export const sessionSecret =
   process.env.SESSION_SECRET || "your_session_secret_here";
